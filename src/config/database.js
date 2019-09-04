13904990eedd0b52ff21dbefe730fc4df28e6846
Luis Eduardo Teixeira <1595102@TCS.com>
2019-09-04 16:12:12 -0300

    First Commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '07hw004552',
+  username: 'tcs',
+  password: 'tata',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '25',
+  secure: false,
+  auth: {
+    user: '8f4496139fd16a',
+    pass: '769a726616c4f7',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
